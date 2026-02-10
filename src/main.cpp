6891efaeca17bd2195c8e7fff1ae5fcfb734f00e
shraddha09107 <shraddha09singh06@gmail.com>
2026-02-10 10:02:44 +0530

    Add FSR reading functionality and configure serial monitor settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
+int fsrPin = A0; // the FSR and 10K pulldown are connected to a0
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
 }
 
 void loop() {
 // write your code here
+    int fsrReading = analogRead(fsrPin); // the voltage at the junction of the FSR and pulldown
+
+    Serial.println(fsrReading);
+    delay(300);
 }
